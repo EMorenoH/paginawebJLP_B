95fda53fa5f634d30d136ab9b3a1753ba21e07dd
EMorenoH <68623282+EMorenoH@users.noreply.github.com>
2020-11-30 14:01:53 -0600

    Add files via upload

---
+++ js/covid19.js
@@ -0,0 +1,8 @@
+//alert(´img src="img/logo05.jpg"´);
+
+
+//alert("Mensaje Covid-19");
+
+
+alert("<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Aspernatur consequuntur placeat perspiciatis et, ducimus iusto, earum tempora corrupti facilis. Ullam sequi, commodi. Deserunt odio qui, non natus minima, dolores et sint enim! Quod explicabo incidunt, excepturi maiores odit deserunt expedita, dolore, aspernatur necessitatibus perspiciatis, corporis minus possimus ratione accusantium voluptatibus.</p></p>");
+
